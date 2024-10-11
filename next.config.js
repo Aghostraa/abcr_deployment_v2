173068acc649620e53c9138fb55ae1c62e580dc4
Aghostraa <ahouraazarbin@gmail.com>
2024-10-11 21:06:05 +0200

    config fix

--- next.config.js
+++ next.config.js
@@ -5,4 +5,13 @@ const nextConfig = {
     },
   }
   
-  module.exports = nextConfig
+  module.exports = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: '/api/:path*',
+        },
+      ]
+    },
+  }
